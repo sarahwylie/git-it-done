2503ac0257797395a96bc29ff8a8dd6c0713a795
sarahwylie <sarahannemariawylie@gmail.com>
2021-12-06 21:07:28 -0600

    full issues list with links

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,24 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
-      };
+    };
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -42,8 +56,12 @@ var getRepoIssues = function (repo) {
         // request was successful
         if (response.ok) {
             response.json().then(function (data) {
-                //pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         }
         else {
@@ -52,4 +70,4 @@ var getRepoIssues = function (repo) {
     });
 };
 
-getRepoIssues("sarahwylie/git-it-done");
+getRepoIssues("facebook/react");
